1c37b499b17254f274e1782d4918165ae73c0111
ABD ALAZEEZ ALYOSFI <alazeez353@gmail.com>
2023-06-30 23:10:51 +0300

    delete ad details

--- lib/feature/ad_details/domain/repo/add_detils_repo.dart
+++
@@ -1,5 +0,0 @@
-import 'package:realstate/feature/ad_details%20%20/infrastructure/models/post.dart';
-
-abstract class AdDetailsRepo {
-  Future<Post> getPost(String id);
-}
--- lib/feature/ad_details/infrastructure/datasources/remote_datasource.dart
+++
@@ -1,9 +0,0 @@
-import 'package:realstate/feature/ad_details%20%20/infrastructure/models/post.dart';
-
-class AdDetailsRemoteDataSource {
-  Future<Post> getPostDetails(String postId) async {
-    // TODO CALL API AND RETURN POST FROM IT
-
-    return Post();
-  }
-}
